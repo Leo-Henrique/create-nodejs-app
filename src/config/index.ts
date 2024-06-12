41b10d44b6c205943406f71652d145ae8ddf2ca3
leo-henrique <leonardo0507.business@gmail.com>
2024-06-12 15:50:39 -0300

    refactor: change folders structure

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,13 @@
 import { env } from "@/env";
 import { resolve } from "path";
 
-const srcPath = resolve(__dirname, "..");
+const rootPath = resolve(__dirname, "../..");
 
 export const GENERATED_APP_ROOT_PATH =
   env.NODE_ENV === "production"
     ? process.cwd()
-    : resolve(srcPath, "generated-apps");
+    : resolve(rootPath, "generated-apps");
 
-export const TEMPLATES_PATH = resolve(srcPath, "templates");
+export const TEMPLATES_PATH = resolve(rootPath, "templates");
 
 export const CLEAN_TEMPLATE_PATH = resolve(TEMPLATES_PATH, "clean");
