e33b61c3c3eaae565b37d18b138eed67ed6c19fb
Leonardo Henrique <leonardo0507.business@gmail.com>
2024-07-20 09:56:15 -0300

    chore(templates): set default all environment variables

--- templates/fastify/src/env.ts
+++ templates/fastify/src/env.ts
@@ -3,8 +3,11 @@ import { z } from "zod";
 
 const schema = z.object({
   NODE_ENV: z.enum(["test", "development", "production"]),
-  API_NAME: z.string(),
-  API_PORT: z.coerce.number(),
+  API_NAME: z
+    .string()
+    .nullable()
+    .default(process.env.npm_package_name ?? null),
+  API_PORT: z.coerce.number().default(3333),
   API_ACCESS_PERMISSION_CLIENT_SIDE: z.string().default("*"),
 });
 
