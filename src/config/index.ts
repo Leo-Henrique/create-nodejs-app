c752a68654ce835923f1a4221593af6ce063372c
Leonardo Henrique <leonardo0507.business@gmail.com>
2024-07-21 14:33:18 -0300

    fix: remove app-root-path

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,28 @@
-import appRootPath from "app-root-path";
-import { existsSync, mkdirSync } from "fs";
+import { existsSync, mkdirSync, readdirSync } from "fs";
 import { resolve } from "path";
 
-if (!process.env.NODE_ENV && process.argv[1].endsWith(".js")) {
-  process.env.NODE_ENV = "production";
-} else {
-  process.env.NODE_ENV = "development";
+if (!process.env.NODE_ENV) {
+  if (process.argv[1].endsWith(".js")) {
+    process.env.NODE_ENV = "production";
+  } else {
+    process.env.NODE_ENV = "development";
+  }
 }
+
+const getPackageJsonUpDir = (dir = __dirname): string => {
+  const filenames = readdirSync(dir, { encoding: "utf-8" });
+
+  if (filenames.includes("package.json")) return dir;
+
+  return getPackageJsonUpDir(resolve(dir, ".."));
+};
+
+const ROOT_PATH = getPackageJsonUpDir();
 
 export const GENERATED_APP_TARGET_ROOT_PATH =
   process.env.NODE_ENV === "production"
     ? process.cwd()
-    : resolve(appRootPath.path, "generated-apps");
+    : resolve(ROOT_PATH, "generated-apps");
 
 if (
   process.env.NODE_ENV !== "production" &&
@@ -20,6 +31,6 @@ if (
   mkdirSync(GENERATED_APP_TARGET_ROOT_PATH);
 }
 
-export const TEMPLATES_PATH = resolve(appRootPath.path, "templates");
+export const TEMPLATES_PATH = resolve(ROOT_PATH, "templates");
 
 export const CLEAN_TEMPLATE_PATH = resolve(TEMPLATES_PATH, "clean");
