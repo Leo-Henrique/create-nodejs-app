409596dad195196ec70c78dd513c11c82964cab0
Leonardo Henrique <leonardo0507.business@gmail.com>
2025-01-11 12:46:40 -0300

    chore(template-react-vite): add custom translation for zod error messages with i18n

--- templates/react-vite/src/main.tsx
+++ templates/react-vite/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import "./index.css";
+import { startZodWithI18n } from "./lib/zod-i18n";
+
+startZodWithI18n();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
