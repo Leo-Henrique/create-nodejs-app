a2c680a3c22df69eaaeffdf06ebd20234a9569f9
leo-henrique <leonardo0507.business@gmail.com>
2025-04-29 13:47:53 -0300

    refactor(template-vite): update zod translation

--- templates/react-vite/src/lib/zod-i18n.ts
+++ templates/react-vite/src/lib/zod-i18n.ts
@@ -2,19 +2,27 @@ import i18next from "i18next";
 import { z } from "zod";
 import { zodI18nMap } from "zod-i18n-map";
 import translation from "zod-i18n-map/locales/pt/zod.json";
-import zodI18nTranslationForEndUsers from "./zod-i18n-translation-for-end-users.json";
+import zodI18nPtBrTranslation from "./zod-i18n-pt-br-translation.json";
 
 export function startZodWithI18n() {
   i18next.init({
     lng: "pt",
     resources: {
       pt: {
-        zod: { ...translation, ...zodI18nTranslationForEndUsers },
+        zod: {
+          ...translation,
+          ...zodI18nPtBrTranslation,
+          errors: {
+            ...translation.errors,
+            ...zodI18nPtBrTranslation.errors,
+          },
+        },
       },
+    },
+    interpolation: {
+      escapeValue: false,
     },
   });
 
   z.setErrorMap(zodI18nMap);
 }
-
-export { z };
