c387d8d86d751b68781c706b7e0dac3ba4f9e2d2
Léo <leo@iMac.local>
2025-06-17 11:27:35 -0300

    fix(template-fastify): read environment variables correctly in production

--- templates/fastify/src/env.ts
+++ templates/fastify/src/env.ts
@@ -22,18 +22,20 @@ if (process.env.npm_lifecycle_event?.includes(":test"))
 if (!process.env.NODE_ENV)
   throw new Error("Could not set to the environment variables to use.");
 
-const envFileName =
-  envFileNames[process.env.NODE_ENV as keyof typeof envFileNames];
+const nodeEnv = process.env.NODE_ENV as keyof typeof envFileNames;
+const envFileName = envFileNames[nodeEnv];
 
 config({
-  path: resolve(__dirname, "..", envFileName),
+  path: resolve(
+    __dirname,
+    nodeEnv === "production" ? "../../" : "..",
+    envFileName,
+  ),
   override: true,
 });
 
 const schema = z.object({
-  NODE_ENV: z
-    .enum(["production", "development", "test"])
-    .default(process.env.NODE_ENV as keyof typeof envFileNames),
+  NODE_ENV: z.enum(["production", "development", "test"]).default(nodeEnv),
   API_NAME: z.string().default(packageJson.name),
   API_PORT: z.coerce.number().default(3333),
   API_ACCESS_PERMISSION_CLIENT_SIDE: z.string().default("*"),
