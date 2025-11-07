510d2f5a6eb20c73c551a59343fd3801f1642f53
Léo <leo@iMac.local>
2025-11-07 19:37:10 -0300

    chore(template-react-vite-tanstack-router): change env setup

--- templates/react-vite-tanstack-router/src/env.ts
+++ templates/react-vite-tanstack-router/src/env.ts
@@ -1,17 +1,14 @@
-import { z } from "zod";
+import { treeifyError, z } from "zod";
 
 const schema = z.object({
-  APP_NAME: z.string(),
-  API_BASE_URL: z
-    .string()
-    .url()
-    .transform((url) => new URL(url)),
+  PUBLIC_APP_NAME: z.string(),
+  PUBLIC_API_BASE_URL: z.url().transform((url) => new URL(url)),
 });
 
 const parsedEnv = schema.safeParse(import.meta.env);
 
 if (!parsedEnv.success) {
-  console.error(parsedEnv.error.flatten().fieldErrors);
+  console.error(treeifyError(parsedEnv.error));
 
   throw new Error("Invalid environment variables.");
 }
